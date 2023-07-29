54526eae0b28dac00111c14b5828af6bb5f8735e
Design User DTP <graficodesign007@gmail.comgit config --global user.name grafico007git config --git config --graficodesign007@gmail.comgit remote add origin https://github.com/grafico007/ultimatehub.git897 git config --global user.email graficodesign007@gmail.com>
2023-07-29 16:49:07 +0530

    first commit

---
+++ forms/contact.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Set the recipient email address
+    $recipient_email = "umthubdubai@gmail.com";
+
+    // Set the email subject
+    $subject = "New Contact Form Submission from $name";
+
+    // Compose the email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Message:\n$message";
+
+    // Set the email headers
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Attempt to send the email
+    if (mail($recipient_email, $subject, $email_content, $headers)) {
+        echo "Thank you for contacting us! We will get back to you shortly.";
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+}
+?>
